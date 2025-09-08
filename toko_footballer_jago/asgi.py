e87e25daaeb282327156c090067eccc79b367637
syafitr4 <daffasyafitra12@gmail.com>
2025-09-08 18:43:24 +0700

    tugas 2

---
+++ toko_footballer_jago/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for toko_footballer_jago project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toko_footballer_jago.settings')
+
+application = get_asgi_application()
