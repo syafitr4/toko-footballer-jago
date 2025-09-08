a7d1f2b3cf91021c2b8c3e66c49696366bd8d66f
syafitr4 <daffasyafitra12@gmail.com>
2025-09-08 19:49:23 +0700

    test revisi

--- main/views.py
+++ main/views.py
@@ -3,10 +3,10 @@ from .models import Product
 
 def show_main(request):
     context = {
-        'app_name': 'Football Shop',
-        'npm': '240123456',      # ganti dengan punyamu
-        'name': 'Haru Urara',    # ganti dengan namamu
-        'class': 'PBP A',        # ganti dengan kelasmu
+        'app_name': 'Toko Footballer Jago',
+        'npm': '2406495546',      # ganti dengan punyamu
+        'name': 'Daffa Syafitra',    # ganti dengan namamu
+        'class': 'PBP B',        # ganti dengan kelasmu
         'products': Product.objects.all(),  # ambil seluruh produk dari DB
     }
     return render(request, "main.html", context)
